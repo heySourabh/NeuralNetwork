d34240938a86672985aeea860c827cf32835ac4b
Sourabh Bhat <heySourabh@gmail.com>
2020-09-23 07:07:32 +0530

    the skeleton code

---
+++ code/nn/NeuralNetwork.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 07:02:58 2020
+
+@author: Sourabh Bhat ( https://spbhat.in/ )
+"""
+
+
+# Neural network class definition
+class NeuralNetwork:
+    
+    # initialize the neural network
+    def __init__(self):
+        pass
+    
+    # train the neural network
+    def train(self):
+        pass
+    
+    # query the neural network
+    def query(self):
+        pass
