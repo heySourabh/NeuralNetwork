140c592b3d72aa573d4833330085199ddba2b81d
Sourabh Bhat <heySourabh@gmail.com>
2020-09-24 07:45:29 +0530

    weights - the heart of the network

--- code/nn/NeuralNetwork.py
+++ code/nn/NeuralNetwork.py
@@ -6,6 +6,7 @@ Created on Wed Sep 23 07:02:58 2020
 @author: Sourabh Bhat ( https://spbhat.in/ )
 """
 
+import numpy as np
 
 # Neural network class definition
 class NeuralNetwork:
@@ -19,6 +20,15 @@ class NeuralNetwork:
         
         # learning rate
         self.lr = learningRate
+        
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is 
+        # from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc.
+        self.wih = np.random.rand(self.hnodes, self.inodes) - 0.5
+        self.who = np.random.rand(self.onodes, self.hnodes) - 0.5
+        
         pass
     
     # train the neural network
