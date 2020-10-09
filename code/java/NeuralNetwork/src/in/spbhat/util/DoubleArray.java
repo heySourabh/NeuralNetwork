349215651173292caf2bacb9d27d73b5eee912e6
Sourabh Bhat <heySourabh@gmail.com>
2020-10-09 10:56:57 +0530

    Added simple implementations of Vector and Matrix

---
+++ code/java/NeuralNetwork/src/in/spbhat/util/DoubleArray.java
@@ -0,0 +1,213 @@
+package in.spbhat.util;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+
+public class DoubleArray {
+    private final static double EPS = 1e-15;
+
+    public static double[] add(double[] a1, double[] a2) {
+        double[] result = new double[a1.length];
+
+        for (int i = 0; i < a1.length; i++) {
+            result[i] = a1[i] + a2[i];
+        }
+
+        return result;
+    }
+
+    public static void increment(double[] array, double[] increment) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] += increment[i];
+        }
+    }
+
+    /**
+     * a1 - a2
+     *
+     * @param a1 array
+     * @param a2 array
+     * @return new array (a1 - a2)
+     */
+    public static double[] subtract(double[] a1, double[] a2) {
+        double[] result = new double[a1.length];
+
+        for (int i = 0; i < a1.length; i++) {
+            result[i] = a1[i] - a2[i];
+        }
+
+        return result;
+    }
+
+    public static void decrement(double[] array, double[] decrement) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] -= decrement[i];
+        }
+    }
+
+    public static double[] multiply(double[] a, double s) {
+        double[] result = new double[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[i] * s;
+        }
+
+        return result;
+    }
+
+    /**
+     * Element-wise multiply
+     *
+     * @param a1 double array
+     * @param a2 double array
+     * @return new array a1 * a2
+     */
+    public static double[] multiply(double[] a1, double[] a2) {
+        return apply(a1, a2, (e1, e2) -> e1 * e2);
+    }
+
+    public static double[] divide(double[] array, double denominator) {
+        return apply(array, e -> divide(e, denominator));
+    }
+
+    /**
+     * Element-wise divide
+     *
+     * @param a1 double array
+     * @param a2 double array
+     * @return new array a1 / a2
+     */
+    public static double[] divide(double[] a1, double[] a2) {
+        return apply(a1, a2, DoubleArray::divide);
+    }
+
+    private static double divide(double numerator, double denominator) {
+        if (Math.abs(denominator) < EPS) {
+            throw new ArithmeticException("divide by zero.");
+        }
+
+        return numerator / denominator;
+    }
+
+    public static double[] abs(double[] array) {
+        return apply(array, Math::abs);
+    }
+
+    public static double[] sqr(double[] array) {
+        return apply(array, e -> e * e);
+    }
+
+    public static double[] apply(double[] a1, double[] a2, DoubleBinaryOperator function) {
+        double[] result = new double[a1.length];
+
+        for (int i = 0; i < a1.length; i++) {
+            result[i] = function.applyAsDouble(a1[i], a2[i]);
+        }
+
+        return result;
+    }
+
+    public static double[] apply(double[] array, DoubleUnaryOperator function) {
+        double[] newArray = new double[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = function.applyAsDouble(array[i]);
+        }
+
+        return newArray;
+    }
+
+    public static double norm2Sqr(double[] array) {
+        return sum(apply(array, e -> e * e));
+    }
+
+    public static double norm2(double[] array) {
+        return Math.sqrt(norm2Sqr(array));
+    }
+
+    public static double sum(double[] array) {
+        double sum = 0.0;
+        for (double e : array) {
+            sum += e;
+        }
+
+        return sum;
+    }
+
+    public static double dot(double[] v1, double[] v2) {
+        double innerProduct = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            innerProduct += v1[i] * v2[i];
+        }
+
+        return innerProduct;
+    }
+
+    /**
+     * Creates a new array by picking minimum of the elements at the index from the two arrays.
+     *
+     * @param a1 array of double
+     * @param a2 array of double
+     * @return Element-wise minimum.
+     */
+    public static double[] min(double[] a1, double[] a2) {
+        return apply(a1, a2, Math::min);
+    }
+
+    /**
+     * Creates a new array by picking maximum of the elements at the index from the two arrays.
+     *
+     * @param a1 array of double
+     * @param a2 array of double
+     * @return Element-wise maximum.
+     */
+    public static double[] max(double[] a1, double[] a2) {
+        return apply(a1, a2, Math::max);
+    }
+
+    public static void copy(double[] from, double[] to) throws ArrayIndexOutOfBoundsException {
+        System.arraycopy(from, 0, to, 0, from.length);
+    }
+
+    public static double[] copyOf(double[] array) {
+        double[] newArray = new double[array.length];
+        copy(array, newArray);
+
+        return newArray;
+    }
+
+    public static double[] zeros(int length) {
+        return new double[length];
+    }
+
+    public static double[] random(int length, Random generator) {
+        double[] rDoubles = new double[length];
+
+        for (int i = 0; i < length; i++) {
+            rDoubles[i] = generator.nextDouble();
+        }
+
+        return rDoubles;
+    }
+
+    public static double[] random(int length, Random generator, double min, double max) {
+        double[] values = new double[length];
+
+        double range = max - min;
+        for (int i = 0; i < length; i++) {
+            values[i] = min + generator.nextDouble() * range;
+        }
+
+        return values;
+    }
+
+    public static double[] newFilledArray(int length, double value) {
+        double[] array = new double[length];
+
+        Arrays.fill(array, value);
+
+        return array;
+    }
+}
---
+++ code/java/NeuralNetwork/src/in/spbhat/util/DoubleMatrix.java
@@ -0,0 +1,372 @@
+package in.spbhat.util;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Use this class only for small matrices for quick prototyping.
+ * May not have efficient algorithms for large matrices.
+ */
+public class DoubleMatrix {
+    public static double[][] create(int rows, int cols) {
+        return new double[rows][cols];
+    }
+
+    public static double[][] createRandom(int rows, int cols, Random generator) {
+        return apply(create(rows, cols), x -> generator.nextDouble());
+    }
+
+    public static double[][] createRandom(int rows, int cols, double mean, double sigma, Random generator) {
+        return apply(create(rows, cols), x -> generator.nextGaussian() * sigma + mean);
+    }
+
+    public static double[][] apply(double[][] matrix, UnaryOperator<Double> operator) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        final double[][] newMatrix = create(numRows, numCols);
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                newMatrix[i][j] = operator.apply(matrix[i][j]);
+            }
+        }
+
+        return newMatrix;
+    }
+
+    public static double[][] add(double[][] A, double[][] B) {
+        int numRows = A.length;
+        int numCols = A[0].length;
+
+        if (numRows != B.length || numCols != B[0].length) {
+            throw new IllegalArgumentException("Size of A != Size of B.");
+        }
+
+        double[][] result = new double[numRows][numCols];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                result[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Subtract matrix B from matrix A => A - B
+     *
+     * @param A matrix
+     * @param B matrix
+     * @return A - B
+     */
+    public static double[][] subtract(double[][] A, double[][] B) {
+        int numRows = A.length;
+        int numCols = A[0].length;
+
+        if (numRows != B.length || numCols != B[0].length) {
+            throw new IllegalArgumentException("Size of A != Size of B.");
+        }
+
+        double[][] result = new double[numRows][numCols];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                result[i][j] = A[i][j] - B[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static double[][] multiply(double[][] matrix, double scalar) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] result = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                result[i][j] = matrix[i][j] * scalar;
+            }
+        }
+
+        return result;
+    }
+
+    public static double[][] multiply(double[][] A, double[][] B) {
+        int numRowsA = A.length;
+        int numColsA = A[0].length;
+        int numRowsB = B.length;
+        int numColsB = B[0].length;
+
+        if (numColsA != numRowsB) {
+            throw new IllegalArgumentException("Number of columns of A != Number of rows of B.");
+        }
+
+        double[][] result = new double[numRowsA][numColsB];
+
+        for (int i = 0; i < numRowsA; i++) {
+            for (int j = 0; j < numColsB; j++) {
+                double sum = 0;
+                for (int k = 0; k < numColsA; k++) {
+                    sum += A[i][k] * B[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+
+        return result;
+    }
+
+    public static double[][] multiply(double[][] matrix1, double[][] matrix2, double[][]... matrices) {
+        double[][] M = multiply(matrix1, matrix2);
+        return Arrays.stream(matrices)
+                .reduce(M, DoubleMatrix::multiply);
+    }
+
+    public static double[] multiply(double[][] matrix, double[] vector) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        if (numCols != vector.length) {
+            throw new IllegalArgumentException("Number of matrix columns != Length of Vector.");
+        }
+
+        double[] result = new double[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < numCols; j++) {
+                sum += matrix[i][j] * vector[j];
+            }
+            result[i] = sum;
+        }
+
+        return result;
+    }
+
+    public static double[][] I(int n) {
+        double[][] identityMatrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            identityMatrix[i][i] = 1.0;
+        }
+
+        return identityMatrix;
+    }
+
+    public static double[] column(double[][] matrix, int col) {
+        int numRows = matrix.length;
+        double[] column = new double[numRows];
+        for (int r = 0; r < numRows; r++) {
+            column[r] = matrix[r][col];
+        }
+
+        return column;
+    }
+
+    public static double[][] transpose(double[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] result = new double[numCols][numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static double determinant(double[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        if (numRows != numCols) {
+            throw new IllegalArgumentException("Not square matrix.");
+        }
+
+        if (numRows == 1) {
+            return matrix[0][0];
+        }
+
+        double det = 0.0;
+        for (int c = 0; c < numCols; c++) {
+            int r = 0;
+            double[][] mat = removeRowColumn(matrix, r, c);
+            det += matrix[r][c] * determinant(mat) * signOf(r, c);
+        }
+
+        return det;
+    }
+
+    public static double[][] invert(double[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        if (numRows != numCols) {
+            throw new IllegalArgumentException("Not square matrix.");
+        }
+
+        double[][] adjoint = transpose(cofactorMatrix(matrix));
+        double determinant = IntStream.range(0, adjoint.length)
+                .mapToDouble(i -> adjoint[0][i] * matrix[i][0])
+                .sum();
+
+        if (Math.abs(determinant) < 1e-15) {
+            throw new ArithmeticException("Matrix is singular.");
+        }
+
+        return multiply(adjoint, 1.0 / determinant);
+    }
+
+    public static double[][] cofactorMatrix(double[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        if (numRows != numCols) {
+            throw new IllegalArgumentException("Not square matrix.");
+        }
+
+        double[][] result = new double[numRows][numCols];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                double[][] m = removeRowColumn(matrix, i, j);
+                result[i][j] = determinant(m) * signOf(i, j);
+            }
+        }
+
+        return result;
+    }
+
+    private static int signOf(int row, int column) {
+        return (row % 2 == 0 ? 1 : -1) * (column % 2 == 0 ? 1 : -1);
+    }
+
+    public static double[][] removeRowColumn(double[][] matrix, int row, int col) {
+        return removeColumn(removeRow(matrix, row), col);
+    }
+
+    public static double[][] removeColumn(double[][] matrix, int col) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] result = new double[numRows][numCols - 1];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0, c = 0; j < numCols; j++) {
+                if (j == col) continue;
+                result[i][c] = matrix[i][j];
+                c++;
+            }
+        }
+
+        return result;
+    }
+
+    public static double[][] removeRow(double[][] matrix, int row) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] result = new double[numRows - 1][numCols];
+
+        for (int i = 0, r = 0; i < numRows; i++) {
+            if (i == row) continue;
+            System.arraycopy(matrix[i], 0, result[r], 0, numCols);
+            r++;
+        }
+
+        return result;
+    }
+
+    public static void copy(double[][] from, double[][] to) {
+        int numRows = from.length;
+        int numCols = from[0].length;
+        for (int i = 0; i < numRows; i++) {
+            System.arraycopy(from[i], 0, to[i], 0, numCols);
+        }
+    }
+
+    public static double[][] copyOf(double[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        double[][] copy = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            System.arraycopy(matrix[i], 0, copy[i], 0, numCols);
+        }
+
+        return copy;
+    }
+
+    public static double[][] swapRows(double[][] matrix, int row1, int row2) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        if (row1 < 0 || row1 >= numRows) throw new IllegalArgumentException("Row index is out of bounds: " + row1);
+        if (row2 < 0 || row2 >= numRows) throw new IllegalArgumentException("Row index is out of bounds: " + row2);
+        if (row1 == row2) return copyOf(matrix);
+
+        double[][] result = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            if (i == row1)
+                System.arraycopy(matrix[row2], 0, result[row1], 0, numCols);
+            else if (i == row2)
+                System.arraycopy(matrix[row1], 0, result[row2], 0, numCols);
+            else
+                System.arraycopy(matrix[i], 0, result[i], 0, numCols);
+        }
+
+        return result;
+    }
+
+    public static double[][] swapColumns(double[][] matrix, int col1, int col2) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        if (col1 < 0 || col1 >= numCols) throw new IllegalArgumentException("Column index is out of bounds: " + col1);
+        if (col2 < 0 || col2 >= numCols) throw new IllegalArgumentException("Column index is out of bounds: " + col2);
+        if (col1 == col2) return copyOf(matrix);
+
+        double[][] result = new double[numRows][numCols];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                int jj = j;
+                if (j == col1)
+                    jj = col2;
+                else if (j == col2)
+                    jj = col1;
+                result[i][jj] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static String stringify(double[][] matrix) {
+        int decimals = 10;
+        int numberWidth = decimals + 10;
+        String startEndDelimiter = IntStream.range(0, numberWidth * matrix[0].length)
+                .mapToObj(i -> "-")
+                .collect(Collectors.joining(""));
+
+        String matrixString = Arrays.stream(matrix)
+                .map(row -> Arrays.stream(row)
+                        .mapToObj(d -> format(d, numberWidth, decimals))
+                        .collect(Collectors.joining("")))
+                .collect(Collectors.joining("\n"));
+
+        return startEndDelimiter + "\n"
+                + matrixString + "\n"
+                + startEndDelimiter;
+    }
+
+    private static String format(double number, int maxPrintWidth, int numDecimals) {
+        if (Math.abs(number) >= 0.1 && Math.abs(number) < 10 || number == 0)
+            return String.format("%-" + maxPrintWidth + "." + numDecimals + "f", number);
+        else
+            return String.format("%-" + maxPrintWidth + "." + numDecimals + "E", number);
+    }
+}
