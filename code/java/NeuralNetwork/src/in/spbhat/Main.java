f6f40d5444420746120814be190617b36bd0506e
Sourabh Bhat <heySourabh@gmail.com>
2020-10-10 18:05:58 +0530

    Refactoring into a separate function

--- code/java/NeuralNetwork/src/in/spbhat/Main.java
+++ code/java/NeuralNetwork/src/in/spbhat/Main.java
@@ -17,18 +17,26 @@ import static in.spbhat.util.DoubleArray.newFilledArray;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        long timeStart = System.currentTimeMillis();
+        double performance = networkPerformance(200, 0.1, 5,
+                new File("../../../mnist_dataset/mnist_train.csv"),
+                new File("../../../mnist_dataset/mnist_test.csv"));
+        System.out.println("Performance = " + performance);
+        long timeStop = System.currentTimeMillis();
+        System.out.println("Time taken = " + (timeStop - timeStart) / 1000.0 + " seconds");
+    }
+
+    private static double networkPerformance(int hiddenNodes, double learningRate, int epochs,
+                                             File trainDataFile, File testDataFile) throws FileNotFoundException {
         int inputNodes = 28 * 28;
-        int hiddenNodes = 100;
         int outputNodes = 10;
-        double learningRate = 0.3;
-        int epochs = 5;
 
         var neuralNetwork = new NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate);
         System.out.println("Training network...");
         for (int epoch = 0; epoch < epochs; epoch++) {
-            try (Scanner trainDataFile = new Scanner(new File("mnist_dataset/mnist_train_100.csv"))) {
-                while (trainDataFile.hasNextLine()) {
-                    MNISTData trainData = new MNISTData(trainDataFile.nextLine());
+            try (Scanner trainDataScanner = new Scanner(trainDataFile)) {
+                while (trainDataScanner.hasNextLine()) {
+                    MNISTData trainData = new MNISTData(trainDataScanner.nextLine());
                     double[] inputs = apply(trainData.getData1D(), e -> e * 0.99 + 0.01);
                     double[] targets = newFilledArray(outputNodes, 0.01);
                     targets[trainData.getLabel()] = 0.99;
@@ -39,9 +47,9 @@ public class Main {
 
         System.out.println("Testing network...");
         List<Integer> scorecard = new ArrayList<>();
-        try (Scanner testDataFile = new Scanner(new File("mnist_dataset/mnist_test_10.csv"))) {
-            while (testDataFile.hasNextLine()) {
-                MNISTData testData = new MNISTData(testDataFile.nextLine());
+        try (Scanner testDataScanner = new Scanner(testDataFile)) {
+            while (testDataScanner.hasNextLine()) {
+                MNISTData testData = new MNISTData(testDataScanner.nextLine());
                 double[] inputs = apply(testData.getData1D(), e -> e * 0.99 + 0.01);
                 double[] outputs = neuralNetwork.query(inputs);
                 int networkLabel = getLabel(outputs);
@@ -49,10 +57,10 @@ public class Main {
                 scorecard.add(networkLabel == correctLabel ? 1 : 0);
             }
         }
-        double performance = scorecard.stream()
+
+        return scorecard.stream()
                 .mapToDouble(i -> i)
                 .sum() / scorecard.size();
-        System.out.println("Performance = " + performance);
     }
 
     private static int getLabel(double[] output) {
