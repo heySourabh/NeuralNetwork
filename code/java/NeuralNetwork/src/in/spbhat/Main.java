aafc7081ce9db9a5422b418e9a1586d3b3df041f
Sourabh Bhat <heySourabh@gmail.com>
2020-10-13 17:40:14 +0530

    Code cleanup

--- code/java/NeuralNetwork/src/in/spbhat/Main.java
+++ code/java/NeuralNetwork/src/in/spbhat/Main.java
@@ -7,19 +7,16 @@ package in.spbhat;
 import in.spbhat.util.MNISTData;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import static in.spbhat.util.DoubleArray.apply;
 import static in.spbhat.util.DoubleArray.newFilledArray;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         long timeStart = System.currentTimeMillis();
         double performance = networkPerformance(200, 0.1, 5,
                 new File("../../../mnist_dataset/mnist_train.csv"),
@@ -30,41 +27,32 @@ public class Main {
     }
 
     private static double networkPerformance(int hiddenNodes, double learningRate, int epochs,
-                                             File trainDataFile, File testDataFile) throws FileNotFoundException {
+                                             File trainDataFile, File testDataFile) throws IOException {
         int inputNodes = 28 * 28;
         int outputNodes = 10;
 
         var neuralNetwork = new NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate);
         System.out.println("Training network...");
-        try {
-            final List<MNISTData> mnistDataList = Files.lines(trainDataFile.toPath())
-                    .map(MNISTData::new)
-                    .collect(Collectors.toUnmodifiableList());
-            for (int epoch = 0; epoch < epochs; epoch++) {
-                System.out.print("Epoch:" + (epoch + 1) + "/" + epochs + "\r");
-                for (MNISTData trainData : mnistDataList) {
-                    double[] inputs = apply(trainData.getData1D(), e -> e * 0.99 + 0.01);
-                    double[] targets = newFilledArray(outputNodes, 0.01);
-                    targets[trainData.getLabel()] = 0.99;
-                    neuralNetwork.train(inputs, targets);
-                }
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            System.out.print("Epoch:" + (epoch + 1) + "/" + epochs + "\r");
+            Files.lines(trainDataFile.toPath()).map(MNISTData::new).forEach(trainData -> {
+                double[] inputs = apply(trainData.getData1D(), e -> e * 0.99 + 0.01);
+                double[] targets = newFilledArray(outputNodes, 0.01);
+                targets[trainData.getLabel()] = 0.99;
+                neuralNetwork.train(inputs, targets);
+            });
         }
 
         System.out.println("Testing network...");
         List<Integer> scorecard = new ArrayList<>();
-        try (Scanner testDataScanner = new Scanner(testDataFile)) {
-            while (testDataScanner.hasNextLine()) {
-                MNISTData testData = new MNISTData(testDataScanner.nextLine());
-                double[] inputs = apply(testData.getData1D(), e -> e * 0.99 + 0.01);
-                double[] outputs = neuralNetwork.query(inputs);
-                int networkLabel = getLabel(outputs);
-                int correctLabel = testData.getLabel();
-                scorecard.add(networkLabel == correctLabel ? 1 : 0);
-            }
-        }
+        Files.lines(testDataFile.toPath()).map(MNISTData::new).forEach(testData -> {
+            double[] inputs = apply(testData.getData1D(), e -> e * 0.99 + 0.01);
+            double[] outputs = neuralNetwork.query(inputs);
+            int networkLabel = getLabel(outputs);
+            int correctLabel = testData.getLabel();
+            scorecard.add(networkLabel == correctLabel ? 1 : 0);
+        });
 
         return scorecard.stream()
                 .mapToDouble(i -> i)
